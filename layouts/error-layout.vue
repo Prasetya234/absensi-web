12d262b7e485c41adaae7e453c55543f7989b892
SECONDTA ARDIANSYAH <idnssquad300@gmail.com>
2023-02-07 09:58:53 +0700

    [Styling] not found page

---
+++ layouts/error-layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="all-not-found">
+    <div class="w-full h-screen flex justify-center items-center">
+      <div class="space-y-8 text-center font-error">
+        <img
+          src="../assets/img/not-found-image.png"
+          alt="Not Found Image"
+          class="w-1/2 mx-auto"
+        />
+        <p class="text-lg text-[#58595B] font-semibold">
+          Access your {{ role }} information system here
+        </p>
+        <button
+          class="px-3 py-2 bg-[#F7931E] rounded-lg text-white capitalize font-semibold"
+          @click="$router.push('/')"
+        >
+          Open {{ role }} Page
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorLayout',
+  data() {
+    return {
+      role: ''
+    };
+  },
+  mounted() {
+    if (localStorage.getItem('role') === 'ADMIN') {
+      this.role = 'dashboard admin';
+    } else if (localStorage.getItem('role') === 'INSTRUCTOR') {
+      this.role = 'dashboard intructor';
+    } else if (localStorage.getItem('role') === 'STUDENT') {
+      this.role = 'dashboard student';
+    } else {
+      this.role = 'login';
+    }
+  }
+};
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "NotFound",
+      layout: 'error-layout', // you can set a custom layout for the error page
+      props: ['error'],
+    }
+  </script>
