e3f2e29ab903830115755a519df15abbf24db112
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2023-02-23 16:44:34 +0700

    update: Style Navbar Admin & Page Absen

--- components/layout/NavbarAdmin.vue
+++ components/layout/NavbarAdmin.vue
@@ -8,30 +8,46 @@
           Absensi X Insctructor
         </p>
       </div>
-      <div class="nav-item">
-        <span class="text-white">Profile</span>
-        <span class="text-red-500" @click="logout">LogOut</span>
+      <div class="nav-item flex alata">
+        <div class="flex gap-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="27"
+            height="27"
+            fill="#F7931E"
+            class=""
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            <path
+              fill-rule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+            />
+          </svg>
+          <span class="text-white">Profile</span>
+        </div>
+        <span class="text-white mx-4">|</span>
+        <span class="text-red-500 cursor-pointer" @click="logout">LogOut</span>
       </div>
     </div>
   </div>
 </template>
 <script>
 import { getUsername, removeAllStorage } from '~/utils/auth';
-
 export default {
   name: 'NavbarAdmin',
-  methods:{
-     logout() {
+  methods: {
+    logout() {
       this.$toast.show(`Good bye ${getUsername()}`, {
-          position: 'top-center',
-          type: 'error',
-          duration: 5000,
-          theme: 'bubble',
-          singleton: true
-        });
-      removeAllStorage()
+        position: 'top-center',
+        type: 'error',
+        duration: 5000,
+        theme: 'bubble',
+        singleton: true
+      });
+      removeAllStorage();
       this.$router.push('/login');
-    },
+    }
   }
 };
 </script>
@@ -40,8 +56,12 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Alata&display=swap');
 
 .nunito {
   font-family: 'Nunito', sans-serif;
+}
+.alata {
+  font-family: 'Alata', sans-serif;
 }
 </style>
