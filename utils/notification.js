4cb371b70285ffa42554a4c652369cd1c840410f
Prasetya234 <putraprasetyadhany@gmail.com>
2023-02-25 11:58:58 +0700

    fix : notif confirm

--- utils/notification.js
+++ utils/notification.js
@@ -1,23 +1,28 @@
 export function notificateAudioPlay() {
-    new Audio('https://assets.mixkit.co/sfx/preview/mixkit-software-interface-back-2575.mp3').play();
+  new Audio('https://assets.mixkit.co/sfx/preview/mixkit-software-interface-back-2575.mp3').play();
 }
 
-export function notificatePopUpPlay({message}) {
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.'); 
-    return;
-  }
-
+export function notificatePopUpPlay({ message }) {
   if (Notification.permission !== "granted")
-    Notification.requestPermission();
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification('Web Absensi', {
+          icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcIdDE42rKH2M-QPAjzpD6jfdW09EKQsfgTg&usqp=CAU',
+          body: message,
+        });
+        notification.onclick = function () {
+          window.open(window.location.origin + "/student/chat");
+        };
+      }
+    })
   else {
     const notification = new Notification('Web Absensi', {
       icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcIdDE42rKH2M-QPAjzpD6jfdW09EKQsfgTg&usqp=CAU',
       body: message,
     });
     notification.onclick = function () {
-      window.open(window.location.origin + "/student/chat");      
+      window.open(window.location.origin + "/student/chat");
     };
   }
-
 }
