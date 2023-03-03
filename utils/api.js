be44f3ec417f4d16d995d33cefcbfe7eacfbbab6
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2023-03-03 10:13:48 +0700

    create table presence

--- utils/api.js
+++ utils/api.js
@@ -1,2 +1,4 @@
-export const API_PRODUCTION = "http://178.128.117.194:55555"
-export const API_WEB_SOCKET = "http://178.128.117.194:55555/ws"
+// export const API_PRODUCTION = "http://178.128.117.194:55555"
+export const API_PRODUCTION = "http://localhost:55555"
+// export const API_WEB_SOCKET = "http://178.128.117.194:55555/ws"
+export const API_WEB_SOCKET = "http://localhost:55555/ws"
